73a82d62e58a2ed8687b3d9da1546a82d318a1cd
JohnSimonetta <john.simonetta@student.northampton.edu>
2024-10-06 22:52:08 +0000

    Message

--- Assignment1/Assignment1_Simonetta.cpp
+++ Assignment1/Assignment1_Simonetta.cpp
@@ -11,9 +11,9 @@ int main()
     //This variable called grade stores the grade I want
     char grade = 'A';
     //This variable called newAge is equal to the variable age + 5
-    double newAge = age + 5
+    double newAge = age + 5;
     //This variable called newHeight is equal to the variable height - 2
-    int newHeight = height - 2
+    int newHeight = height - 2;
 
     //These are the statements where the variables will be put
     cout <<"My name is: "<< name <<endl;
