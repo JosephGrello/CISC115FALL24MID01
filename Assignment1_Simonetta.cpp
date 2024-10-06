19ea2cc5289b1cb85922ba05810bfedf0c1f28c6
JohnSimonetta <john.simonetta@student.northampton.edu>
2024-10-06 22:46:10 +0000

    Message

---
+++ Assignment1_Simonetta.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //This variable called name stores my first name John
+    string name = "John";
+    //This variable called age stores the number of my age
+    int age = 18;
+    //This variable called height stores my height as a decimal
+    int height = 5.11;
+    //This variable called grade stores the grade I want
+    char grade = 'A';
+    //This variable called newAge is equal to the variable age + 5
+    double newAge = age + 5
+    //This variable called newHeight is equal to the variable height - 2
+    int newHeight = height - 2
+
+    //These are the statements where the variables will be put
+    cout <<"My name is: "<< name <<endl;
+    cout <<"Age: " << age <<endl;
+    cout <<"Height: " << height <<endl;
+    cout <<"I want Grade in CISC115: " << grade << endl;
+    cout <<"New Age (double): " << newAge <<endl;
+    cout <<"New Height (int): " <<newHeight <<endl;
+    cout <<"My first basic program in C++" <<endl;
+}
